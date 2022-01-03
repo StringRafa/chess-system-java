bce2478e99d9d64c556fc759a04f1b4fb7d029bb
Rafael <rafa.jo.souza@hotmail.com>
2022-01-03 17:54:31 -0300

    Fix - better treatment to promotion

--- src/chess/ChessMatch.java
+++ src/chess/ChessMatch.java
@@ -1,6 +1,5 @@
 package chess;
 
-import java.security.InvalidParameterException;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.Collectors;
@@ -94,7 +93,7 @@ public class ChessMatch {
 		if (movedPiece instanceof Pawn) {
 			if (movedPiece.getColor() == Color.WHITE && target.getRow() == 0 || movedPiece.getColor() == Color.BLACK && target.getRow() == 7) {
 				promoted = (ChessPiece)board.piece(target);
-				promoted = replacePromotedPiece("Q");
+				promoted = replacePromotedPiece("P");
 			}
 		}
 		
@@ -122,7 +121,7 @@ public class ChessMatch {
 			throw new IllegalStateException("There is no piece to be promoted");
 		}
 		if (!type.equals("B") && !type.equals("N") && !type.equals("R") && !type.equals("Q")) {
-			throw new InvalidParameterException("Invalid type for promotion");
+			return promoted;
 		}
 		
 		Position pos = promoted.getChessPosition().toPosition();
